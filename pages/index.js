a34c6dca4d713f7e5f7542701346e564029ba273
Nickolas Ribeiro <nickolasleosantos@hotmail.com>
2026-02-23 11:08:25 -0300

    add pages folder

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '2px' }}>
+            <h1>oi mozinho</h1>
+            <p>fiz esse deploy, só pra dizer que</p>
+            <p style={{ color: 'red', fontWeight: 'bold', fontSize: '40px', marginTop: '0px' }}>amo você</p>
+        </div>
+    )
+}
+
+export default Home;
